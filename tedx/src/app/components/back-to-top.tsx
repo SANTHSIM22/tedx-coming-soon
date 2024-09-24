9d3bdc916264d4349edc3db3997bab9817566f56
vyshnav <vyshnavravi599@gmail.com>
2024-09-24 19:46:38 +0530

    refactor: Add BacktoTopButton component

---
+++ tedx/src/app/components/back-to-top.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { GoArrowUp } from "react-icons/go";
+
+export const BacktoTopButton = () => {
+    return (
+        <div
+            className="h-16  fixed z-50 bottom-24 md:bottom-12 flex w-screen justify-end items-center"
+            onClick={() => {
+                window.scrollTo({
+                    top: 0,
+                    behavior: "smooth",
+                });
+            }}
+        >
+            <button className="h-16 w-16 rounded-full flex justify-center items-center mr-5">
+                <GoArrowUp size={45} />
+            </button>
+        </div>
+    );
+};
