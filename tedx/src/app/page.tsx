de8097bbf583d5a4a9ffeded4aceea873673a778
vyshnav <vyshnavravi599@gmail.com>
2024-09-25 14:11:09 +0530

    refactor: Add loading spinner to Home component

--- tedx/src/app/page.tsx
+++ tedx/src/app/page.tsx
@@ -4,8 +4,14 @@ import { Header } from "./components/header";
 import { Gallery } from "./components/gallery";
 
 import { BacktoTopButton } from "./components/back-to-top";
+import { useState } from "react";
+import { RotateLoader } from "react-spinners";
 
 export default function Home() {
+    const [loading, setLoading] = useState(true);
+    setTimeout(() => {
+        setLoading(false);
+    }, 3000);
     return (
         <>
             <div className="relative min-h-screen ">
@@ -16,14 +22,20 @@ export default function Home() {
 
                 {/* Main Content */}
                 <div>
-                    <Hero />
+                    {loading ? (
+                        <div className="w-screen h-screen flex justify-center items-center">
+                            <RotateLoader color="#EB0028" />
+                        </div>
+                    ) : (
+                        <Hero />
+                    )}
 
                     {/* <Footer /> */}
                 </div>
             </div>
             <section id="gallery-section">
                 <div className="w-screen h-screen  flex  justify-center no-scrollbar ">
-                    <div className="w-screen  flex justify-center no-scrollbar flex-col">
+                    <div className="w-screen  flex justify-center no-scrollbar flex-row md:flex-col">
                         <Gallery />
                     </div>
                 </div>
