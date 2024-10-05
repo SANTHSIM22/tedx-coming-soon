6b2de7b3305d394cf7994e06de09226e2a6027ed
SANTHSIM22 <santhsimviriledsouza123456@gmail.com>
2024-10-05 23:58:00 +0530

    fix: Dots_vq function

--- tedx/src/app/components/start-loader.tsx
+++ tedx/src/app/components/start-loader.tsx
@@ -2,7 +2,7 @@
 import { motion } from "framer-motion";
 //======================================
 export const Dots_v1 = () => (
-  <div className="relative flex items-center mr-10 ">
+  <div className="relative flex items-center ">
     <motion.span
       initial={{ scale: 0 }}
       animate={{ scale: 1 }}
